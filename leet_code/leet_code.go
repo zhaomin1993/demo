38d3e503a53bc56a5788cbb21bf4af29e4855903
zhaom6 <zhaom6@knownsec.com>
2022-08-17 19:36:13 +0800

    add: code

--- leet_code/leet_code.go
+++ leet_code/leet_code.go
@@ -1,7 +1,10 @@
 package leet_code
 
 import (
+	"fmt"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 func longestCommonPrefix(strs []string) string {
@@ -447,4 +450,65 @@ func trap(height []int) int {
 		ans += h1 + h2 - v
 	}
 	return ans - len(height)*h1
+}
+
+/*
+给定一个单词数组 words 和一个长度 maxWidth ，重新排版单词，使其成为每行恰好有 maxWidth 个字符，且左右两端对齐的文本。
+
+你应该使用 “贪心算法” 来放置给定的单词；也就是说，尽可能多地往每行中放置单词。必要时可用空格 ' ' 填充，使得每行恰好有 maxWidth 个字符。
+
+要求尽可能均匀分配单词间的空格数量。如果某一行单词间的空格不能均匀分配，则左侧放置的空格数要多于右侧的空格数。
+
+文本的最后一行应为左对齐，且单词之间不插入额外的空格。
+*/
+func fullJustify(words []string, maxWidth int) []string {
+	var (
+		length      = 0
+		res         = make([]string, 0, len(words)/2)
+		temp        = make([]string, 0, 2)
+		maxWidthStr = strconv.Itoa(maxWidth)
+	)
+	for _, v := range words {
+		newL := length + len(v)
+		if newL < maxWidth {
+			length += len(v) + 1
+			temp = append(temp, v)
+		} else if newL == maxWidth {
+			temp = append(temp, v)
+			res = append(res, strings.Join(temp, " "))
+			length = 0
+			temp = make([]string, 0, 2)
+		} else {
+			l := len(temp)
+			gap := l + maxWidth - length
+			if l == 1 {
+				res = append(res, fmt.Sprintf("%-"+maxWidthStr+"s", temp[0]))
+			} else {
+				div := gap / (l - 1)
+				mod := gap % (l - 1)
+				if l == 2 {
+					mod = 0
+				}
+				var s string
+				for i := range temp {
+					space := div
+					if i < mod {
+						space++
+					}
+					s += temp[i]
+					if i != len(temp)-1 {
+						s += fmt.Sprintf("%"+strconv.Itoa(space)+"s", "")
+					}
+				}
+				res = append(res, s)
+			}
+			length = len(v) + 1
+			temp = make([]string, 0, 2)
+			temp = append(temp, v)
+		}
+	}
+	if len(temp) > 0 {
+		res = append(res, fmt.Sprintf("%-"+maxWidthStr+"s", strings.Join(temp, " ")))
+	}
+	return res
 }
